863864eb0a5c1c2472f98944872b26185396793a
fizzbuzz <elim.liu@mail.utoronto.ca>
2025-05-15 19:27:41 -0400

    edit3

--- src/Multiples.java
+++ src/Multiples.java
@@ -1,2 +1,16 @@
 public class Multiples {
+    public static void main(String[] args) {
+        int count = 0;
+
+        // Loop through all numbers below 1000
+        for (int i = 0; i < 1000; i++) {
+            // Check if the number is a multiple of 3 or 5
+            if (i % 3 == 0 || i % 5 == 0) {
+                count++;
+            }
+        }
+
+        // Print the result
+        System.out.println("There are " + count + " multiples of 3 or 5 below 1000.");
+    }
 }
